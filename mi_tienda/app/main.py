92e1e077b12de32b7ee5f9b03e4922cd5749e590
andrew-quezada <a.quezadar@duocuc.cl>
2024-11-12 21:17:36 -0300

    agregar manejo de solicitudes
    se agrega la interfaz de manejo de solicitudes, antes de ocupar se deben hacer cambios en la base de datos generando nuevas tablas para su buen funcionamiento

--- mi_tienda/app/main.py
+++ mi_tienda/app/main.py
@@ -6,11 +6,12 @@ def main():
     # Inicializa la conexión a la base de datos y guarda la conexión
     conn = create_connection()
 
-        # Configura la ventana principal de la aplicación con ttkbootstrap
+    # Configura la ventana principal de la aplicación con ttkbootstrap
     root = ttk.Window(themename="superhero")
     app = Login(root)
-        
-        # Inicia el bucle principal de la aplicación
+
+    # Inicia el bucle principal de la aplicación
     root.mainloop()
+
 if __name__ == "__main__":
     main()
